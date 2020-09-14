6aa349151f29477b5b7d101b08ca6192683f5997
Warren Niu <warrenniu@gmail.com>
2020-09-14 16:49:03 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -22,8 +22,8 @@ def update_status
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
   # Code your solution here:
-epic_tragedy[:hero][status:] = "dead"
-epic_tragedy[:heroine][status:] = "dead"
+epic_tragedy[:hero][:status] = "dead"
+epic_tragedy[:heroine][:status] = "dead"
 
 
 
